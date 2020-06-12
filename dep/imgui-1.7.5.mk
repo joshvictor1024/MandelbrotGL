6c48b43ac257eeff6f18d74cea3b9e9cb711766c
joshvictor1024 <joshvictoronfire@gmail.com>
2020-06-12 22:20:45 +0800

    Untracked some files

--- dep/imgui-1.7.5.mk
+++
@@ -1,5 +0,0 @@
-DEP += $(strip $(patsubst $(DEPDIR)/imgui-1.7.5/%.cpp, $(INTDIR)/%.d, $(wildcard $(DEPDIR)/imgui-1.7.5/*.cpp)))
-
-$(INTDIR)/%.d: $(DEPDIR)/imgui-1.7.5/%.cpp
-	$(CXX) -MM -MF $@ $(DEPDIR)/imgui-1.7.5/$*.cpp $(INCDIRFLAGS) -MT $(INTDIR)/$*.o
-	echo		$(CXX) $(CXXFLAGS) $(DEPDIR)/imgui-1.7.5/$*.cpp -c $(INCDIRFLAGS) -o $$@ >> $@
